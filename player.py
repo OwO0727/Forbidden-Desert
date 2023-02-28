c0d4acf9ff88396520ae030ce8c64271c4fff16b
shayne-c07 <125975283+shayne-c07@users.noreply.github.com>
2023-02-28 12:25:29 +0000

    Update player.py

--- player.py
+++ player.py
@@ -1,17 +1,48 @@
 class player:
-    def __init__(self,name,water):
+    def __init__(self, name, water):
         name = self.name
         water = self.water
 
+
 class archeologist(player):
-    def __init__(self,name,water):
-        super().__init__(name,water)
-    
-    def clear2sand(self,tile):
-        #Method to clear 2 sand from any tile
-        
+    def __init__(self, name, water):
+        super().__init__(name, water)
+
+    def clear2sand(self, tile):
+# Method to clear 2 sand from any tile
+
+
 class climber(player):
-    def __init__(self,name,water):
-        super().__init__(name,water)
-    
-    def 
+    def __init__(self, name, water):
+        super().__init__(name, water)
+        
+    def movetoblockedtile(self, tile, player):
+
+
+class explorer(player):
+    def __init__(self, name, water):
+        super().__init__(name, water)
+
+    def diagonal(self, tile):
+
+class meteorologist(player):
+    def __init__(self, name, water):
+        super().__init__(name, water)
+        
+    def movestormcard(self, card):
+    def drawlesscards(self, cardnum):
+
+class navigator(player):
+    def __init__(self, name, water):
+        super().__init__(name, water)
+        
+    def moveplayer(self, tile):
+
+class water_carrier(player):
+    def __init__(self, name, water):
+        super().__init__(name, water)
+        
+    def take2water(self, tile):
+    def give_water(self, player):
+
+
