60983384bf11d3980f68690499deddf0da901cb4
DPschool <125975348+DPschool@users.noreply.github.com>
2023-02-28 14:23:20 +0000

    Update Difficulty selection.py

--- Difficulty selection.py
+++ Difficulty selection.py
@@ -3,24 +3,21 @@
 from tkinter import *
 from tkinter import ttk
 
-playerCount = 2 #Placeholder
-
-def setupSandStormTimer(difficulty,playerCount):
-    # Calculate stormLevel using some calculation involving the number of players and the difficulty
-    print("Difficulty:",difficulty,"Players:",playerCount) # Placeholder
+def setupSandStormTimer(difficulty):
+    stormLevel = difficulty
 
 root = Tk()
 
 # Window size
-root.geometry("500x500")
+root.geometry("1280x720")
 
-label = ttk.Label(root,text="Select Difficulty")
-label.pack()
+ttk.Label(root,text="Select Difficulty").pack()
+
 
 # The difficulty buttons
-ttk.Button(root,text="Novice",command=lambda: setupSandStormTimer(0,playerCount)).pack()
-ttk.Button(root,text="Normal",command=lambda: setupSandStormTimer(1,playerCount)).pack()
-ttk.Button(root,text="Elite",command=lambda: setupSandStormTimer(2,playerCount)).pack()
-ttk.Button(root,text="Legendary",command=lambda: setupSandStormTimer(3,playerCount)).pack()
+ttk.Button(root,text="Novice",command=lambda: setupSandStormTimer(0)).pack()
+ttk.Button(root,text="Normal",command=lambda: setupSandStormTimer(1)).pack()
+ttk.Button(root,text="Elite",command=lambda: setupSandStormTimer(2)).pack()
+ttk.Button(root,text="Legendary",command=lambda: setupSandStormTimer(3)).pack()
 
 root.mainloop()
