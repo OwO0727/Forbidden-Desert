099f8e56120bfef2d0f02bed1669e34a6fcda906
shayne-c07 <125975283+shayne-c07@users.noreply.github.com>
2023-02-28 12:56:55 +0000

    Update playernumber.py

--- playernumber.py
+++ playernumber.py
@@ -9,10 +9,13 @@ root = Tk()
 root.wm_title("Forbidden Island: Menu")
 root.geometry(f"{windowDims[0]}x{windowDims[1]}")
 
+
 bg = PhotoImage(file = "img/Menu.png")
 canvas1 = Canvas( root, width = 1280, height = 720)
 canvas1.pack(fill = "both", expand = True)
 canvas1.create_image( 640, 0, image = bg, anchor = "n")
+canvas1.create_text(640, 220, text="How Many Players?", fill="black", font=('Helvetica 18 bold'))
+canvas1.pack()
 
 frm = ttk.Frame(root, padding=10)
 
@@ -34,9 +37,8 @@ p4.place(x=windowDims[0] // 2 + 0, y=windowDims[1] // 2 -100, width=50, height=5
 p5 = Button(root, text="5", command = five)
 p5.place(x=windowDims[0] // 2 + 50, y=windowDims[1] // 2 -100, width=50, height=50)
 
-canvas= Canvas(root, width= 300, height= 100, bg="red")
-canvas.create_text(150, 50, text="How Many Players?", fill="white", font=('Helvetica 15 bold'))
-canvas.pack()
+
 root.mainloop()
+
 
 
