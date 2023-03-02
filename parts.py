943145ee505e1439ea894f322ece74917ab671f0
DPschool <125975348+DPschool@users.noreply.github.com>
2023-03-02 16:54:43 +0000

    Update parts.py

--- parts.py
+++ parts.py
@@ -1,5 +1,16 @@
 class part:
-    def __init__(self,location,collected):
-        location = self.location
+    def __init__(self,x,y,collected):
+        x = self.x
+        y = self.y
         collected = self.collected
+    
+    def setX(self,x):
+        self.x = x
+    
+    def setY(self,y):
+        self.y = y
 
+propeller = part(-1,-1,False)
+engine = part(-1,-1,False)
+solarCrystal = part(-1,-1,False)
+navigationDeck = part(-1,-1,False)
